ac70def75e30b4567785999314717e685f1945e4
Roman <maistrenko.roman@gmail.com>
2025-07-18 15:39:01 +0300

    18.07.2025

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,121 @@
-import {useState} from 'react'
+import React, { useMemo, useState } from 'react'
 import './App.css'
-import TaskElement from "./Components/Task/TaskElement"
-import Input from "./Components/Input/Input"
-import { Task } from "./Types/Types"
+import TaskElement from './Components/Task/TaskElement'
+import Input from './Components/Input/Input'
+import { Task } from './Types/Types'
 
 function App() {
-    const [tasks, setTasks] = useState<Task[]>([])
-    const [taskForm, setTaskForm] = useState<Omit<Task, 'done' | 'dueDate' | 'id'>>({
+  const [tasks, setTasks] = useState<Task[]>([])
+  const [taskForm, setTaskForm] = useState<
+    Omit<Task, 'done' | 'dueDate' | 'id'>
+  >({
+    title: '',
+    description: '',
+    dateStart: '',
+    dateEnd: '',
+  })
+  const [sortedTasks, setSortedTasks] = useState<Task[]>([])
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    const { name, value } = e.target
+    setTaskForm((prev) => ({ ...prev, [name]: value }))
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    const id = crypto.randomUUID()
+    const newTask: Task = {
+      ...taskForm,
+      id: id,
+      done: false,
+      dueDate: '',
+    }
+    setTasks((prev) => [...prev, newTask])
+    setTaskForm({
       title: '',
       description: '',
       dateStart: '',
-      dateEnd: ''
+      dateEnd: '',
     })
-    const [sortedTasks, setSortedTasks] = useState<Task[]>([]);
+  }
 
-    const handleChange = (e: React.ChangeEvent<HTMLFormElement | HTMLTextAreaElement>) => {
-        const {name, value} = e.target;
-        setTaskForm(prev => ({ ...prev, [name]: value }) )
-    };
+  const toggleCompleteTask = (id: string) => {
+    setTasks((prevTasks) =>
+      prevTasks.map((task) =>
+        task.id === id
+          ? { ...task, done: !task.done, dueDate: new Date().toISOString() }
+          : task,
+      ),
+    )
+  }
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-        const id = crypto.randomUUID();
-        const newTask: Task = {
-            ...taskForm,
-            id: id,
-            done: false,
-            dueDate: '',
-        };
-        setTasks(prev => [ ...prev, newTask]);
-        setTaskForm({
-            title: '',
-            description: '',
-            dateStart: '',
-            dateEnd: ''
-        });
-    }
+  const deleteTask = (id: string) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
-    const toggleCompleteTask = (id: string) => {
-        setTasks(prevTasks =>
-            prevTasks.map(task =>
-                task.id === id ? {...task, done: !task.done, dueDate: new Date().toISOString()} : task
-            )
+  const parseDate = (date: string) => {
+    const [day, month, year] = date.split('-').map(Number)
+    return new Date(year, month - 1, day)
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <Input
+          type={'text'}
+          placeholder={'Task Name'}
+          name={'title'}
+          onChange={handleChange}
+          value={taskForm.title}
+        >
+          Task Name:
+        </Input>
+        <Input
+          type={'text'}
+          placeholder={'Task Description'}
+          name={'description'}
+          onChange={handleChange}
+          value={taskForm.description}
+        >
+          Task Description:
+        </Input>
+        <Input
+          type={'date'}
+          name={'dateStart'}
+          onChange={handleChange}
+          value={taskForm.dateStart}
+          max={taskForm.dateEnd}
+        >
+          Start Date:
+        </Input>
+        <Input
+          type={'date'}
+          name={'dateEnd'}
+          onChange={handleChange}
+          value={taskForm.dateEnd}
+          min={taskForm.dateStart}
+        >
+          End Date:
+        </Input>
+        <button type="submit">Add Task</button>
+      </form>
+      {[...tasks]
+        .sort(
+          (a, b) =>
+            parseDate(a.dateStart).getTime() - parseDate(b.dateStart).getTime(),
         )
-    }
-
-    const deleteTask = (id: string) => {
-        setTasks(tasks.filter((task) => task.id !== id))
-    }
-
-    const parseDate = (date: string) => {
-        const [day, month, year] = date.split('-').map(Number);
-        return new Date(year, month-1, day);
-    };
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <Input type={"text"} placeholder={"Task Name"} name={"title"} onChange={handleChange} value={taskForm.title}>
-                    Task Name:
-                </Input>
-                <Input type={"text"} placeholder={"Task Description"} name={"description"} onChange={handleChange} value={taskForm.description}>
-                    Task Description:
-                </Input>
-                <Input type={"date"} name={"dateStart"} onChange={handleChange} value={taskForm.dateStart} max={taskForm.dateEnd}>
-                    Start Date:
-                </Input>
-                <Input type={"date"} name={"dateEnd"} onChange={handleChange} value={taskForm.dateEnd} min={taskForm.dateStart}>
-                    End Date:
-                </Input>
-                <button type="submit">
-                    Add Task
-                </button>
-            </form>
-            {[...tasks]
-            .sort((a, b) => parseDate(a.dateStart).getTime() - parseDate(b.dateStart).getTime())
-            .map((task: Task, index: number) => (
-                <TaskElement
-                    key={task.id}
-                    task={task}
-                    deleteTask={deleteTask}
-                    toggleComplete={toggleCompleteTask}
-                >
-                </TaskElement>
-            ))}
-        </div>
-    )}
+        .map((task: Task, index: number) => (
+          <TaskElement
+            key={task.id}
+            task={task}
+            deleteTask={deleteTask}
+            toggleComplete={toggleCompleteTask}
+          ></TaskElement>
+        ))}
+    </div>
+  )
+}
 
 export default App
--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -1,33 +1,44 @@
-import React from "react";
+import React from 'react'
 
 type InputProps = {
-    type: string;
-    placeholder?: string;
-    children?: React.ReactNode;
-    value: string;
-    onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-    name: string;
-    max?: string;
-    min?: string;
+  type: string
+  placeholder?: string
+  children?: React.ReactNode
+  value: string
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => void
+  name: string
+  max?: string
+  min?: string
 }
 
-const Input = ({ type, placeholder, value, children, onChange, name, min, max }: InputProps) => {
-    return (
-        <div>
-            <label>
-                {children}
-                <input
-                    value={value}
-                    type={type}
-                    placeholder={placeholder}
-                    name={name}
-                    onChange={onChange}
-                    max={max}
-                    min={min}
-                />
-            </label>
-        </div>
-    );
-};
+const Input = ({
+  type,
+  placeholder,
+  value,
+  children,
+  onChange,
+  name,
+  min,
+  max,
+}: InputProps) => {
+  return (
+    <div>
+      <label>
+        {children}
+        <input
+          value={value}
+          type={type}
+          placeholder={placeholder}
+          name={name}
+          onChange={onChange}
+          max={max}
+          min={min}
+        />
+      </label>
+    </div>
+  )
+}
 
-export default Input;
+export default Input
